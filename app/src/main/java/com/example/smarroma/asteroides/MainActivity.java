4de62d2f00a5a3f5ed2f61fa8bced093ec542d9b
Sergio225 <s3rg10.martinez.roman@gmail.com>
2018-03-22 15:48:45 +0100

    Initial commit Asteroides

---
+++ app/src/main/java/com/example/smarroma/asteroides/MainActivity.java
@@ -0,0 +1,19 @@
+package com.example.smarroma.asteroides;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.widget.TextView;
+
+public class MainActivity extends AppCompatActivity {
+
+    private TextView tv1;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        tv1 = (TextView)findViewById(R.id.tv1);
+
+    }
+}
